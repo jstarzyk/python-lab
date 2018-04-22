ed033b19747b3dd6e1925b04d403c77354fdc163
jakub <jstarzyk98@gmail.com>
2018-04-22 19:53:51 +0200

    Add validation with tests

---
+++ exp_simplifier/tests/tests1.py
@@ -0,0 +1,16 @@
+import unittest
+from exp_simplifier.validate import validate
+
+
+class TestValidity(unittest.TestCase):
+    def test_validity(self):
+        self.assertEqual(validate('(a | b) & c'), True)
+        self.assertEqual(validate('(aaa | b) & c'), True)
+        self.assertEqual(validate('a | b & c'), True)
+        self.assertEqual(validate('a | b) & c'), False)
+        self.assertEqual(validate('a | 1 & 0'), True)
+        self.assertEqual(validate('a | 1 & >'), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ exp_simplifier/validate.py
@@ -0,0 +1,62 @@
+import string
+from enum import Enum
+
+# variables = string.ascii_lowercase
+alphanumeric = string.ascii_letters + string.digits
+operators = '&|^~>='
+
+
+class State(Enum):
+    S1 = 1
+    S2 = 2
+
+
+def is_variable(var):
+    for s in var:
+        if s not in alphanumeric:
+            return False
+    return True
+
+
+def consume(expr, i):
+    for j in range(i + 1, i + len(expr[i:])):
+        if not expr[j].isalpha():
+            return expr[i:j], j
+    return expr[i:i+1], i + 1
+    # res = expr[i:].split(' ', 1)
+    # return res[0], i + len(res[0])
+
+
+def validate(expr):
+    state = State.S1
+    parens = 0
+
+    i = 0
+    while i < len(expr):
+        if expr[i] == ' ':
+            i += 1
+            continue
+        if state == State.S1:
+            if expr[i] == '(':
+                parens += 1
+                i += 1
+            else:
+                var, i = consume(expr, i)
+                if is_variable(var):
+                    state = State.S2
+                else:
+                    return False
+        elif state == State.S2:
+            if expr[i] == ')':
+                parens -= 1
+                i += 1
+            else:
+                if expr[i] in operators:
+                    state = State.S1
+                    i += 1
+                else:
+                    return False
+        if parens < 0:
+            return False
+    return parens == 0 and state == State.S2
+
