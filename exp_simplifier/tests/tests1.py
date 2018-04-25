5201eccc720ad7ea34b23bfb088df37549f3ebb7
jakub <jstarzyk98@gmail.com>
2018-04-25 19:39:43 +0200

    Add implicant tables; create expressions

--- exp_simplifier/tests/tests1.py
+++ exp_simplifier/tests/tests1.py
@@ -23,9 +23,9 @@ class TestValidity(unittest.TestCase):
     def test_get_min_terms(self):
         expr = '(a | b) & c'
         template, variables = validate(expr)
-        self.assertEqual(get_min_terms(template, variables), [((3,), (0, 1, 1)),
+        self.assertEqual(get_min_terms(template, variables), ([((3,), (0, 1, 1)),
                                                               ((5,), (1, 0, 1)),
-                                                              ((7,), (1, 1, 1))])
+                                                              ((7,), (1, 1, 1))], None))
 
     def test_diff_by_one(self):
         self.assertEqual(diff_by_one((1, 0, None), (1, 0, None)), (1, 0, None))
@@ -36,13 +36,13 @@ class TestValidity(unittest.TestCase):
     # def test_create_groups(self):
     #     self.assertEqual()
 
-    def test_merge_groups(self):
+    def test_merge_two_groups(self):
         g1 = [((2,), (0, 0, 1, 0)), ((8,), (1, 0, 0, 0))]
         g2 = [((6,), (0, 1, 1, 0)), ((9,), (1, 0, 0, 1)), ((10,), (1, 0, 1, 0))]
-        self.assertEqual(merge_groups(g1, g2), {((2, 6), (0, None, 1, 0)),
-                                                ((2, 10), (None, 0, 1, 0)),
-                                                ((8, 9), (1, 0, 0, None)),
-                                                ((8, 10), (1, 0, None, 0))})
+        self.assertEqual(merge_two_groups(g1, g2), {((2, 6), (0, None, 1, 0)),
+                                                    ((2, 10), (None, 0, 1, 0)),
+                                                    ((8, 9), (1, 0, 0, None)),
+                                                    ((8, 10), (1, 0, None, 0))})
 
     def test_create_groups(self):
         self.assertEqual(collections.OrderedDict({2: {((3,), (0, 1, 1)), ((5,), (1, 0, 1))},
@@ -55,13 +55,30 @@ class TestValidity(unittest.TestCase):
         mm = [2, 6, 8, 9, 10, 11, 14, 15]
         min_terms = sorted((((m,), tuple([int(d) for d in format(m, '0%db' % 4)])) for m in mm), key=lambda x: x[0][0])
         groups = create_groups(min_terms)
-        ccat = concat_groups(concat_groups(groups))
+        ccat = merge_groups(merge_groups(groups))
         d = collections.OrderedDict()
         d[1] = {((2, 6, 10, 14), (None, None, 1, 0)),
                 ((8, 9, 10, 11), (1, 0, None, None))}
         d[2] = {((10, 11, 14, 15), (1, None, 1, None))}
-    #
+        #
         self.assertEqual(ccat, d)
+
+    def test_covered_by_one(self):
+        self.assertEqual(covered_by_one([1, 1, None]), -1)
+        self.assertEqual(covered_by_one([1, None, None]), 0)
+        self.assertEqual(covered_by_one([None, None, None]), -1)
+        self.assertEqual(covered_by_one([None, None, 1]), 2)
+
+    def test_simplify(self):
+        self.assertEqual(simplify('a | b', None, None), 'b | a')
+        self.assertEqual(simplify('(a | b) & ~b', None, None), 'a & ~b')
+        self.assertEqual(simplify('a | ~a', None, None), True)
+        self.assertEqual(simplify('a | a', None, None), 'a')
+        self.assertEqual(simplify('a & ~a', None, None), False)
+        self.assertEqual(simplify('1', None, None), True)
+        self.assertEqual(simplify('0 | 1', None, None), True)
+        self.assertEqual(simplify('a > b', None, None), '~a | b')
+        # self.assertEqual(simplify('a | ~a', None, None), 'True')
 
     # def test_tmp(self):
     #     self.assertEqual(re.findall('(\d\w)+', '~a|1&b\^(~c)'), ('a', 1, 'b', 'c'))
@@ -69,6 +86,7 @@ class TestValidity(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    # unittest.
     # mm = [2, 6, 8, 9, 10, 11, 14, 15]
     # min_terms = sorted((((m,), tuple([int(d) for d in format(m, '0%db' % 4)])) for m in mm), key=lambda x: x[0][0])
     # print(min_terms)
--- exp_simplifier/validate.py
+++ exp_simplifier/validate.py
@@ -8,10 +8,13 @@ from functools import partial
 class Infix(object):
     def __init__(self, func):
         self.func = func
+
     def __or__(self, other):
         return self.func(other)
+
     def __ror__(self, other):
         return Infix(partial(self.func, other))
+
     def __call__(self, v1, v2):
         return self.func(v1, v2)
 
@@ -23,7 +26,7 @@ def _xor(a, b):
 
 @Infix
 def _imp(a, b):
-    return not a and b
+    return not a or b
 
 
 @Infix
@@ -51,7 +54,7 @@ def consume_variable(expr, i):
     for j in range(i + 1, i + len(expr[i:])):
         if not expr[j].isalpha():
             return expr[i:j], j
-    return expr[i:i+1], i + 1
+    return expr[i:i + 1], i + 1
 
 
 def validate(expr):
@@ -128,14 +131,23 @@ def validate(expr):
 def get_min_terms(_template, _variables):
     min_terms = []
 
+    i = 0
     for m in range(2 ** len(_variables)):
         bins = [int(d) for d in format(m, '0%db' % len(_variables))]
         values = [d == 1 for d in bins]
         _globals = {'_xor': _xor, '_imp': _imp, '_equ': _equ}
         if eval(_template, _globals, dict(zip(_variables, values))):
             min_terms.append(((m,), tuple(bins)))
+            i += 1
 
-    return min_terms
+    if i == 0:
+        always_evaluates_to = False
+    elif i == 2 ** len(_variables):
+        always_evaluates_to = True
+    else:
+        always_evaluates_to = None
+
+    return min_terms, always_evaluates_to
 
 
 def diff_by_one(bits1, bits2):
@@ -173,7 +185,7 @@ def create_groups(min_terms):
     return collections.OrderedDict(sorted(groups.items(), key=lambda i: i[0]))
 
 
-def merge_groups(g1, g2):
+def merge_two_groups(g1, g2):
     # g1 = [([2], [0,0,1,0]), ([8], [1,0,0,0])]
     result = set()
 
@@ -188,30 +200,148 @@ def merge_groups(g1, g2):
     return result
 
 
-def concat_groups(groups):
+def merge_groups(groups):
     result = collections.OrderedDict()
 
     keys = list(groups)
 
     for i in range(len(keys) - 1):
         # result.append(merge_groups(groups[i], groups[i + 1]))
-        res = merge_groups(groups[keys[i]], groups[keys[i + 1]])
+        res = merge_two_groups(groups[keys[i]], groups[keys[i + 1]])
         if res:
             result[keys[i]] = res
 
     return result
 
 
-# if __name__ == '__main__':
-#     # e1 = '(a | b)'
-#     # e1 = '(aaa | b) & c'
-#     # e2 = 'a | 1b & 0'
-#     # e2 = ')a | 1b & 0'
-#     e3 = '~x|1&b^(~c)'
-#     # print(validate(e1))
-#     # print(validate(e2))
-#     # print(validate(e3))
-#     _template, variables = validate(e3)
-#     variables = sorted(variables)
-#     print(_template, variables)
-#     print(get_min_terms(_template, variables))
+# def reduce_table():
+
+
+def create_prime_implicant_table(groups, sv_min_terms):
+    merged_min_terms = []
+    for group in groups.values():
+        for min_term in group:
+            merged_min_terms.append(min_term)
+
+    # pi_table = []
+    pi_table = collections.OrderedDict()
+
+    for sv_min_term in sv_min_terms:
+        covers_min_term = []
+        sv_min_term_val = sv_min_term[0][0]
+        covers_n = 0
+        for min_term in merged_min_terms:
+            if sv_min_term_val in min_term[0]:
+                covers_min_term.append(1)
+                covers_n += 1
+            else:
+                covers_min_term.append(None)
+        # pi_table.append([sv_min_term_val, covers_min_term])
+        pi_table.setdefault(sv_min_term_val, [])
+        pi_table[sv_min_term_val] = covers_min_term
+
+    return pi_table, merged_min_terms
+
+
+def covered_by_one(min_term):
+    i = 0
+    r = -1
+    for c in range(len(min_term)):
+        if min_term[c] == 1:
+            r = c
+            i += 1
+        if i > 1:
+            return -1
+    return r
+
+
+def get_prime_implicants(groups, min_terms):
+    out = []
+
+    table, merged_min_terms = create_prime_implicant_table(groups, min_terms)
+    while len(table) > 0:
+        pi = None
+        app = None
+
+        for t in table.items():
+            pi = covered_by_one(t[1])
+            if pi != -1:
+                app = merged_min_terms.pop(pi)
+                out.append(app)
+                break
+
+        if pi is None or app is None:
+            return out
+
+        del_mt = app[0]
+        for mt in del_mt:
+            if mt in table.keys():
+                table.pop(mt)
+
+        for t in table.items():
+            t[1].pop(pi)
+
+    return out
+
+
+def make_expr(prime_implicants, variables):
+    or_joined = []
+    for p in prime_implicants:
+        and_joined = []
+        bits = p[1]
+        for i in range(len(bits)):
+            if bits[i] is None:
+                continue
+            elif bits[i] == 0:
+                and_joined.append('~' + variables[i])
+            elif bits[i] == 1:
+                and_joined.append(variables[i])
+        # or_joined.append('(' + ' & '.join(and_joined) + ')')
+        or_joined.append(' & '.join(and_joined))
+
+    return ' | '.join(or_joined)
+
+
+def simplify(expr, mt, vr):
+    if expr:
+        template, variables = validate(expr)
+
+        if template is None or variables is None:
+            # print('%s is not a valid logical expression' % expr)
+            return None
+
+        min_terms, always_evaluates_to = get_min_terms(template, variables)
+
+        if always_evaluates_to is not None:
+            return always_evaluates_to
+    else:
+        min_terms = mt
+        variables = vr
+
+    groups = create_groups(min_terms)
+    while True:
+        res = merge_groups(groups)
+        if res == collections.OrderedDict():
+            break
+        else:
+            groups = res
+
+    prime_implicants = get_prime_implicants(groups, min_terms)
+
+    return make_expr(prime_implicants, variables)
+
+
+def main():
+    # examples = []
+    # examples.append('1 | 0')
+    # examples.append('a | b')
+    # examples.append('b | ~b')
+    print(simplify('a & ~a', None, None))
+
+
+if __name__ == '__main__':
+    main()
+    # mm = [2, 6, 8, 9, 10, 11, 14, 15]
+    # min_terms = sorted((((m,), tuple([int(d) for d in format(m, '0%db' % 4)])) for m in mm), key=lambda x: x[0][0])
+    # print(simplify(None, min_terms, ['W', 'X', 'Y', 'Z']))
+
